d0b59dedde66eba1df2552916f9584c0ccb83926
Raymond Cuya <raymondcuya@gmail.com>
2022-06-30 21:04:05 +1000

    moved input to its own component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 
 import './App.css';
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -45,13 +46,8 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search profile"
-          onChange={onSearchChange}
-        />
-        <CardList profiles={profiles} />
+        <SearchBox onChangeHandler={onSearchChange} />
+        <CardList profiles={filteredProfiles} />
       </div>
     );
   }
