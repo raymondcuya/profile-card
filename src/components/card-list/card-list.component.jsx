ed44114edde466e57fd99e606e82bc3b99dfff62
Raymond Cuya <raymondcuya@gmail.com>
2022-07-07 20:27:38 +1000

    display list of profile

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,24 @@
 import { Component } from 'react';
 
+import './card-list.styles.css';
 class CardList extends Component {
   render() {
     const { profiles } = this.props;
     return (
-      <div>
-        {profiles.map((profile) => (
-          <h1 key={profile.id}>{profile.name}</h1>
-        ))}
+      <div className="card-list">
+        {profiles.map((profile) => {
+          const { name, email, id } = profile;
+          return (
+            <div className="card-container">
+              <img
+                alt={`profile ${profile.name}`}
+                src={`https://robohash.org/${profile.id}?set=set5&size=180x180`}
+              />
+              <h2>{profile.name}</h2>
+              <p>{profile.email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
