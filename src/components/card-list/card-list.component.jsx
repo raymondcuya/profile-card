2e45ed8039365a3e5465dbbf33219ab1755bfd72
Raymond Cuya <raymondcuya@gmail.com>
2022-07-15 19:36:16 +1000

    card list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Card from '../card/card.component';
 
 import './card-list.styles.css';
 class CardList extends Component {
@@ -7,17 +8,7 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {profiles.map((profile) => {
-          const { name, email, id } = profile;
-          return (
-            <div className="card-container">
-              <img
-                alt={`profile ${profile.name}`}
-                src={`https://robohash.org/${profile.id}?set=set5&size=180x180`}
-              />
-              <h2>{profile.name}</h2>
-              <p>{profile.email}</p>
-            </div>
-          );
+          return <Card profile={profile} />;
         })}
       </div>
     );
