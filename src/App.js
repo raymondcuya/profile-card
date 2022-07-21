acd59b8466a84aaba75f042f61cb080b1ee2c746
Raymond Cuya <raymondcuya@gmail.com>
2022-07-21 21:15:58 +1000

    use effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
@@ -7,6 +8,22 @@ import './App.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
+  const [profiles, setProfiles] = useState([]);
+  const [filteredProfiles, setFilteredProfiles] = useState(profiles);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setProfiles(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredProfiles = profiles.filter((profile) => {
+      return profile.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredProfiles(newFilteredProfiles);
+  }, [profiles, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
@@ -21,7 +38,7 @@ const App = () => {
         placeholder="search profiles"
         className="search-box"
       />
-      {/* <CardList profiles={filteredProfiles} /> */}
+      <CardList profiles={filteredProfiles} />
     </div>
   );
 };
