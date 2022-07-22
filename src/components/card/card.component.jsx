8c1971532be13c038179bfe027f32d3ec454b4f5
Raymond Cuya <raymondcuya@gmail.com>
2022-07-22 21:05:11 +1000

    card component uses a function instad of class

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
-
 import './card.styles.css';
 
-class Card extends Component {
-  render() {
-    const { id, name, email } = this.props.profile;
-    return (
-      <div className="card-container">
-        <img
-          alt={`profile ${name}`}
-          src={`https://robohash.org/${id}?set=set5&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const Card = ({ profile }) => {
+  const { id, name, email } = profile;
+
+  return (
+    <div className="card-container">
+      <img
+        alt={`profile ${name}`}
+        src={`https://robohash.org/${id}?set=set5&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
