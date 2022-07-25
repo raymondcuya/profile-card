160b3860e4518da08e990c7f0c4cbd84240edb80
Raymond Cuya <raymondcuya@gmail.com>
2022-07-25 20:47:56 +1000

    converted to function

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,13 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 
 import './card-list.styles.css';
-class CardList extends Component {
-  render() {
-    const { profiles } = this.props;
-    return (
-      <div className="card-list">
-        {profiles.map((profile) => {
-          return <Card profile={profile} />;
-        })}
-      </div>
-    );
-  }
-}
+
+const CardList = ({ profiles }) => (
+  <div className="card-list">
+    {profiles.map((profile) => {
+      return <Card profile={profile} />;
+    })}
+  </div>
+);
 
 export default CardList;
